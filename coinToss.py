aecf564d123606ba10343b80417ca9054c52ecf8
Jessica Tapley <jjtapley@aol.com>
2019-09-14 20:01:13 +0100

    Initial commit

---
+++ coinToss.py
@@ -0,0 +1,27 @@
+import random
+
+money = 100
+
+print("Hello, and welcome to the coin toss game!")
+print("What is your name? :")
+name=input()
+print("Hello, "+name)
+print("How much would you like to bet (max. bet £100)? ")
+bet=input()
+print("Heads or Tails? ")
+guess = input().lower().strip()
+flip=random.randint(1,2)
+
+
+def coin_toss(guess,bet):
+    # Heads is 1, Tails is 2
+    if guess == "heads" and flip==1:
+        print("Heads! You win £"+bet)
+    if guess=="tails" and flip==1:
+        print("Heads... You lose £"+bet)
+    if guess=="tails" and flip==2:
+        print("Tails! You Win "+bet)
+    if guess=="heads" and flip==2:
+        print("Tails... You lose £"+bet)
+                  
+print(coin_toss(guess,bet))
